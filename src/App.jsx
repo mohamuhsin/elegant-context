167dbbe681e217445993119a9372352694bc25ce
mohamuhsin <muhsinohoho@outlook.com>
2024-07-10 14:24:59 +0300

    we have linked the context to state by giving the providing context a value and then used the context in other parts of our app

--- src/App.jsx
+++ src/App.jsx
@@ -67,8 +67,13 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
+
   return (
-    <CartContext.Provider value={{ items: [] }}>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
